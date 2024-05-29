8116dd1026d3c315066f22d5e896c2f95f2ad38c
Alexandru2328 <Alexandru.romario07@gmail.com>
2024-05-29 18:44:12 +0300

    Add project files.

---
+++ PasteBin/Data/PasteBinContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PasteBin.Models;
+
+namespace PasteBin.Data
+{
+    public class PasteBinContext : DbContext
+    {
+        public PasteBinContext (DbContextOptions<PasteBinContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PasteBin.Models.Notes> Notes { get; set; } = default!;
+    }
+}
