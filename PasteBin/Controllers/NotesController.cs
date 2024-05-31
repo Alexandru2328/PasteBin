59a499b8bc93e8aceaf8a5b705516c492f6c198a
Alexandru2328 <Alexandru.romario07@gmail.com>
2024-05-31 11:51:59 +0300

    test

--- PasteBin/Controllers/NotesController.cs
+++ PasteBin/Controllers/NotesController.cs
@@ -19,130 +19,62 @@ namespace PasteBin.Controllers
             _context = context;
         }
 
-        // GET: Notes1
         public async Task<IActionResult> Index()
         {
             return View(await _context.Notes.ToListAsync());
         }
 
-        // GET: Notes1/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var notes = await _context.Notes
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (notes == null)
-            {
-                return NotFound();
-            }
-
+            var notes = await _context.Notes.FirstOrDefaultAsync(m => m.Id == id);
             return View(notes);
         }
 
-        // GET: Notes1/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Notes1/Create
       
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Title,Description")] Notes notes)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(notes);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(notes);
+            _context.Add(notes);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
-        // GET: Notes1/Edit/5
+    
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
             var notes = await _context.Notes.FindAsync(id);
-            if (notes == null)
-            {
-                return NotFound();
-            }
             return View(notes);
         }
 
-        // POST: Notes1/Edit
         
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description")] Notes notes)
         {
-            if (id != notes.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(notes);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!NotesExists(notes.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(notes);
+            _context.Update(notes);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
-        // GET: Notes1/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var notes = await _context.Notes
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (notes == null)
-            {
-                return NotFound();
-            }
-
+            var notes = await _context.Notes.FirstOrDefaultAsync(m => m.Id == id);
             return View(notes);
         }
 
-        // POST: Notes1/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var notes = await _context.Notes.FindAsync(id);
-            if (notes != null)
-            {
-                _context.Notes.Remove(notes);
-            }
-
+            _context.Notes.Remove(notes);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
